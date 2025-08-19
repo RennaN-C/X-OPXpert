0c1a63d53fa48fd68baac4ec0fb8e5a192cb7e58
Marlon Myszka <ens-rennancardoso@ugv.edu.br>
2025-08-19 20:49:01 -0300

    Alterações feitas para página 404

--- X OPXpert/index.js
+++ X OPXpert/index.js
@@ -14,29 +14,12 @@ app.get('/infos',(req,res)=>{
     res.send("Aqui voce encontra informações sobre o servidor")
 })
 
-app.get('/cliente/:nome/:sobrenome',(req,res)=>{
-    res.send(`Seja bem vindo ${req.params.nome} ${req.params.sobrenome}`)
+
+app.get('/404',(req,res)=>{
+    res.sendFile(__dirname+'/404.html')
 })
 
-app.get('/cadastro',(req,res)=>{
-    res.sendFile(__dirname+'/cadastro.html')
-})
 
-app.get('/menos/:val1/:val2',(req,res)=>{
-  res.send(`O valor da subtração é: ${calculos.subtracao(req.params.val1,req.params.val2)}`)
-})
-
-app.get('/multi/:val1/:val2',(req,res)=>{
-  res.send(`O valor da multiplicação é: ${calculos.multiplicacao(req.params.val1,req.params.val2)}`)
-})
-
-app.get('/viagem/:distancia/:velocidade',(req,res)=>{
-  res.send(`O tempo de viagem é: ${calculos.divisao(req.params.distancia,req.params.velocidade)}`)
-})
-
-app.get('/gorjeta/:conta/:porcentagem',(req,res)=>{
-  res.send(`O valor da conta com gorjeta é: ${calculos.porcentagem(req.params.conta,req.params.porcentagem)}`)
-})
 
 app.listen(port, () => {
   console.log(`App de exemplo esta rodando na porta ${port}`)
